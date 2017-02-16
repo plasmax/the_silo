8c44f3ef046090b361e99b2a09a10e7b0c05f218
Florian Einfalt <florian.einfalt@me.com>
2017-02-16 12:43:06 +0000

    Fix a test coverage bug

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,6 @@ import pytest
 
 dir_ = os.path.dirname(__file__)
 pytest.main(['{0}/tests/'.format(dir_),
-             '--cov=nukecontexts',
+             '--cov=the_silo',
              '--cov-report',
              'term-missing'])
