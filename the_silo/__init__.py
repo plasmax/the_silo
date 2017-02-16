2b822525df212c6e27db3fdd3fe8e050fba79f9e
Florian Einfalt <florian.einfalt@me.com>
2017-02-16 12:10:54 +0000

    Add wrapper module for loading functions and gizmos

--- the_silo/__init__.py
+++ the_silo/__init__.py
@@ -1,10 +1,12 @@
 import os
 import re
 import sys
+import json
 import platform
 
+TESTING = False
 try:
-    os.environ['DOCS_CONTEXT']
+    TESTING = os.environ['DOCS_CONTEXT']
 except:
     if platform.system() == 'Darwin':
         application = r'Nuke\d+\.\d+v\d+.app'
@@ -15,31 +17,38 @@ except:
 
     match = re.search(application, sys.executable)
     if not match:
-        raise RuntimeError('Import nukecontexts from within Nuke')
+        raise RuntimeError('Import the_silo from within Nuke')
+    import nuke
 
 __version__ = '0.1.0'
 __all__ = []
 
-import nuke
-import json
-
+silo_name = 'The Silo'
 silo_location = os.path.dirname(os.path.abspath(__file__))
 
-nuke.pluginAddPath('{0}/gizmos'.format(silo_location))
+def build_silo():
+    nuke.pluginAddPath('{0}/gizmos'.format(silo_location))
 
-MENU_NAME = 'The Silo'
-silo_menu = nuke.menu('Nuke').addMenu(MENU_NAME)
+    silo_menu = nuke.menu('Nuke').addMenu(silo_name)
 
-with open('{0}/silo_data.json'.format(silo_location), 'r') as fp:
-    silo_data = json.load(fp)
+    with open('{0}/silo_data.json'.format(silo_location), 'r') as fp:
+        silo_data = json.load(fp)
 
-for gizmo_name, gizmo in silo_data['gizmos']:
-    silo_menu.addCommand('Gizmos/{0}'.format(gizmo_name),
-                         'nuke.nodes.{0}()'.format(gizmo))
+    for gizmo_name, gizmo in silo_data['gizmos']:
+        silo_menu.addCommand('Gizmos/{0}'.format(gizmo_name),
+                             'from the_silo import wrapper;'
+                             'wrapper.create_gizmo(\'{0}\')'.
+                             format(gizmo))
 
-for script_name, script in silo_data['scripts']:
-    silo_menu.addCommand('Scripts/{0}'.format(script_name), str(script))
+    for script_name, module, func in silo_data['scripts']:
+        silo_menu.addCommand('Scripts/{0}'.format(script_name),
+                             'from the_silo import wrapper;'
+                             'wrapper.exec_script(\'{0}\', \'{1}\')'.
+                             format(module, func))
 
-silo_menu.addSeparator()
-silo_menu.addCommand('Version', 'nuke.message("The Silo version {0}")'.format(__version__))
-silo_menu.addCommand('Contribute', 'import webbrowser;webbrowser.open(\'https://github.com/florianeinfalt/the_silo\')')
+    silo_menu.addSeparator()
+    silo_menu.addCommand('Version', 'nuke.message("The Silo version {0}")'.format(__version__))
+    silo_menu.addCommand('Contribute', 'import webbrowser;webbrowser.open(\'https://github.com/florianeinfalt/the_silo\')')
+
+if not TESTING:
+    build_silo()
